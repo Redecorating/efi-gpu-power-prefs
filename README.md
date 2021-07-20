d53e4e13958aa52de0527b15b0a559af8b7a783e
Redecorating <69827514+Redecorating@users.noreply.github.com>
2021-07-20 15:51:46 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# efi-gpu-power-prefs
+
+Set the boot gpu on dual gpu macbooks to the iGPU. I'm assuming you are using grub, and have installed `gnu-efi`.
+
+1. [Spoof apple-set-os](https://wiki.t2linux.org/guides/hybrid-graphics/#enabling-the-igpu), you do not need to do the nvram thing (this repo does it for you)
+2.  Install this:
+	```
+	make
+	sudo cp gpu-power-prefs-x86_64.efi /boot/
+	sudo cp 41_apple_gpu-power-prefs /etc/grub.d/
+	sudo grub-mkconfig -o /boot/grub/grub.cfg
+	```
+3. In the grub menu, select the new option, it will power down and then you can boot again with the igpu.
+4. If you boot macOS, you will need to repeat step 3.
---
+++ gpu-power-prefs.c
@@ -0,0 +1,22 @@
+#include <efi.h>
+#include <efilib.h>
+
+EFI_STATUS EFIAPI efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+
+    const EFI_GUID applegpuGUID = { 0xfa4ce28d, 0xb62f, 0x4c99, {0x9c, 0xc3, 0x68, 0x15, 0x68, 0x6e, 0x30, 0xf9}};
+
+    const UINT32 flags = EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS|EFI_VARIABLE_NON_VOLATILE;
+
+    static const char csrVal[4] = {0x01, 0x00, 0x00, 0x00};
+   
+    EFI_RUNTIME_SERVICES* rt; 
+
+    InitializeLib(ImageHandle, SystemTable);
+    rt = SystemTable->RuntimeServices;
+
+    uefi_call_wrapper(rt->SetVariable, 5, L"gpu-power-prefs", &applegpuGUID, flags, 4, csrVal);
+
+    uefi_call_wrapper(rt->ResetSystem, 4, EfiResetShutdown, EFI_SUCCESS, 0, NULL);
+
+    return EFI_SUCCESS;
+}
